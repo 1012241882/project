9b9e0a701046b384f6786fcb0ea9952dab60e1ed
kaikai1997_com <kaikai19970818@qq.com>
2020-08-24 14:32:16 +0800

    大萨达

---
+++ src/main/java/com/sy/ProjectApplication.java
@@ -0,0 +1,23 @@
+package com.sy;
+
+import com.sy.interceptor.InterceptorRules;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = "com.sy")
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableTransactionManagement
+@Import(value = {SpringShiroConfig.class,SpringEsConfig.class,InterceptorRules.class})
+@EnableElasticsearchRepositories(basePackages="com.sy.es")
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sy/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sy.repository;
+
+
+import com.sy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Integer,User> {
+
+}
